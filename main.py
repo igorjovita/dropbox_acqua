fc178feb603b53f3713f8f3bbd23e821ddfbb119
IgorJovita <igor.jovita@hotmail.com>
2025-09-27 20:20:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,6 @@ data = st.date_input('Insira a data da planilha', format='DD/MM/YYYY')
 if st.button('Pesquisar'):
 
     caminho, nome_planilha = base.gerador_de_caminho(data)
-    st.table(base.baixar(caminho, nome_planilha))
+    st.dataframe(base.baixar(caminho, nome_planilha), hide_index=True)
 
 
